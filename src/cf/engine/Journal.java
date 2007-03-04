94598508874dd63b3184f139b6a25b65165e265a
Sander Kooijmans <sander@gogognome.nl>
2007-03-04 20:47:00 +0000

    Added implementation for equals() and hashCode(). Allow only 1 party per journal.

--- src/cf/engine/Journal.java
+++ src/cf/engine/Journal.java
@@ -1,10 +1,11 @@
 /*
- * $Id: Journal.java,v 1.9 2007-02-10 16:28:46 sanderk Exp $
+ * $Id: Journal.java,v 1.10 2007-03-04 20:47:00 sanderk Exp $
  *
  * Copyright (C) 2006 Sander Kooijmans
  */
 package cf.engine;
 
+import java.util.Arrays;
 import java.util.Currency;
 import java.util.Date;
 import java.util.Locale;
@@ -35,35 +36,40 @@ public class Journal implements Comparable {
      * @param items the items of the journal. The sums of the debet 
      *        and credit amounts must be equal!
      * @throws IllegalArgumentException if the sum of debet and credit amounts differ
+     *          or if more than one item with a party has been specified.
      */
-    public Journal(String id, String description, Date date, JournalItem[] items)
-    {
+    public Journal(String id, String description, Date date, JournalItem[] items) {
         this.id = id;
         this.description = description;
         this.date = date;
         this.items = items;
         
+        int nrParties = 0;
         Currency currency = Database.getInstance().getCurrency();
         Amount totalDebet = Amount.getZero(currency);
         Amount totalCredit = totalDebet;
-        for (int i=0; i<items.length; i++)
-        {
-            if (items[i].isDebet())
-            {
-                totalDebet = totalDebet.add(items[i].getAmount());
+        for (int i=0; i<items.length; i++) {
+            if (items[i].getParty() != null) {
+                nrParties++;
             }
-            else
-            {
+            if (items[i].isDebet()) {
+                totalDebet = totalDebet.add(items[i].getAmount());
+            } else {
                 totalCredit = totalCredit.add(items[i].getAmount());
             }
         }
-        if (totalDebet.compareTo(totalCredit) != 0)
-        {
+        
+        if (totalDebet.compareTo(totalCredit) != 0) {
             AmountFormat af = new AmountFormat(Locale.getDefault());
             throw new IllegalArgumentException(
-                    "The sum of debet and credit amounts differ!" + 
-                    " debet: " + af.formatAmount(totalDebet) + 
+                    "The sum of debet and credit amounts differ for journal " + id 
+                    + "! debet: " + af.formatAmount(totalDebet) + 
                     "; credit: " + af.formatAmount(totalCredit));
+        }
+        
+        if (nrParties > 1) {
+            throw new IllegalArgumentException("More than 1 party has been specified" +
+            		" for journal " + id);
         }
     }
     
@@ -129,4 +135,26 @@ public class Journal implements Comparable {
         Journal that = (Journal)o;
         return this.date.compareTo(that.date);
     }
+    
+    /**
+     * Checks whether this instance is equal to another instance.
+     * @param o the other instance
+     * @return <code>true</code> if this instance is equal to <code>o</code>;
+     *          <code>false</code> otherwise
+     */
+    public boolean equals(Object o) {
+        if (o instanceof Journal) {
+            Journal that = (Journal) o;
+            return this.id.equals(that.id) && this.date.equals(that.date) &&
+            	this.description.equals(that.description) &&
+            	Arrays.equals(this.items, that.items);
+        } else {
+            return false;
+        }
+    }
+    
+    public int hashCode() {
+        return id.hashCode() + date.hashCode() + description.hashCode();
+    }
+    
 }
