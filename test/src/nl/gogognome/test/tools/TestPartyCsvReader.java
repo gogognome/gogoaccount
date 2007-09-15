ee8d77edc25108d7e5b2a768ff23f231406ca72d
Sander Kooijmans <sander@gogognome.nl>
2007-09-15 18:59:21 +0000

    File name is no longer required.

--- test/src/nl/gogognome/test/tools/TestPartyCsvReader.java
+++ test/src/nl/gogognome/test/tools/TestPartyCsvReader.java
@@ -1,12 +1,11 @@
 /*
- * $Id: TestPartyCsvReader.java,v 1.2 2007-09-04 19:05:39 sanderk Exp $
+ * $Id: TestPartyCsvReader.java,v 1.3 2007-09-15 18:59:21 sanderk Exp $
  *
  * Copyright (C) 2007 Sander Kooijmans
  */
 package nl.gogognome.test.tools;
 
 import java.awt.print.PrinterException;
-import java.io.File;
 import java.io.IOException;
 
 import nl.gogognome.cf.tool.ui.PartyCsvSettingsView;
@@ -33,11 +32,9 @@ public class TestPartyCsvReader {
     static TextLabel[] labels;
     
     public static void main(String[] args) throws IOException, PrinterException {
-        String fileName = args[0];
         viewContainer = new ViewContainer("Party label printer");
         frame = new ViewFrame(viewContainer);
-        pcsView = new PartyCsvSettingsView(new File(fileName),
-            "gen.continue", "gen.cancel");
+        pcsView = new PartyCsvSettingsView("gen.continue", "gen.cancel");
         pcsView.addViewListener(new ViewListener() {
             public void onViewClosed(View view) {
                 onClosePartyCsvSettingsView();
