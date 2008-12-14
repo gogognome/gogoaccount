aa0e4d830b6826810da54b2deac2dcf18a88aa52
Sander Kooijmans <sander@gogognome.nl>
2008-12-14 19:32:23 +0000

    Added tool for generating HTML fragments for a birthday calendar.

---
+++ src/nl/gogognome/cf/tool/BirthdayCalendarOfParties.java
@@ -0,0 +1,112 @@
+/*
+ * $Id: BirthdayCalendarOfParties.java,v 1.1 2008-12-14 19:32:23 sanderk Exp $
+ *
+ * Copyright (C) 2005 Sander Kooijmans
+ *
+ */
+
+package nl.gogognome.cf.tool;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Locale;
+
+import nl.gogognome.text.TextResource;
+import nl.gogognome.util.DateUtil;
+import cf.engine.Database;
+import cf.engine.Party;
+import cf.engine.XMLFileReader;
+import cf.engine.XMLParseException;
+
+/**
+ * This class implements a small application that prints a birthday calendar of all parties.
+ */
+public class BirthdayCalendarOfParties {
+
+    /**
+     * @param args should have size 1. The argument contains the XML file containing the
+     *        parties.
+     */
+    public static void main(String[] args) {
+        String xmlFileName = null;
+        
+        // First set the language so that the error messages caused by parsing the other
+        // arguments are shown in the specified language.
+        TextResource tr = TextResource.getInstance();
+        for (int i=0; i<args.length; i++) {
+            if (args[i].startsWith("-lang=")) { 
+                tr.setLocale(new Locale(args[i].substring(6)));
+            }
+        }
+        
+        for (int i=0; i<args.length; i++) {
+            if (args[i].startsWith("-xml-file=")) {
+                if (xmlFileName != null) {
+                    System.out.println(tr.getString("printAge.moreThanOneXmlFile"));
+                    printUsage();
+                    return;
+                }
+                xmlFileName = args[i].substring(10);
+            }
+        }
+
+        if (xmlFileName == null) {
+            System.out.println(tr.getString("printAge.noXmlFile"));
+            printUsage();
+            return;
+        }
+        
+        ArrayList<Party> partiesWithBirthdate = new ArrayList<Party>();
+        try {
+            Database db = XMLFileReader.createDatabaseFromFile(xmlFileName);
+            Party[] parties = db.getParties();
+            for (int i = 0; i < parties.length; i++) {
+                if (parties[i].getBirthDate() != null && !"oud-lid".equals(parties[i].getType())) {
+                    partiesWithBirthdate.add(parties[i]);
+                }
+            }
+        } catch (XMLParseException e) {
+            System.out.println(tr.getString("printAge.syntaxError", e.getMessage()));
+        } catch (IOException e) {
+            System.out.println(tr.getString("printAge.ioError", e.getMessage()));
+        } 
+        Collections.sort(partiesWithBirthdate, new Comparator<Party>() {
+            public int compare(Party p1, Party p2) {
+                int m1 = DateUtil.getField(p1.getBirthDate(), Calendar.MONTH);
+                int d1 = DateUtil.getField(p1.getBirthDate(), Calendar.DATE);
+                int m2 = DateUtil.getField(p2.getBirthDate(), Calendar.MONTH);
+                int d2 = DateUtil.getField(p2.getBirthDate(), Calendar.DATE);
+                if (m1 != m2) {
+                    return m1 - m2;
+                } else {
+                    return d1 - d2;
+                }
+            }
+        });
+        
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MMMMMM", new Locale("nl"));
+        SimpleDateFormat dayOfMonthFormat = new SimpleDateFormat("d", new Locale("nl"));
+        int prevMonth = -1;
+        for (Party party : partiesWithBirthdate) {
+            int month = DateUtil.getField(party.getBirthDate(), Calendar.MONTH);
+            if (prevMonth != month) {
+                System.out.println("\t\t\t\t<tr><td></td><td><br><b>" + monthFormat.format(party.getBirthDate()) + "</b></td></tr>");
+            }
+            System.out.print("\t\t\t\t<tr><td>");
+            System.out.print(dayOfMonthFormat.format(party.getBirthDate()) + "</td><td>" + party.getName());
+            System.out.println("</td></tr>");
+            prevMonth = month;
+        }
+    }
+
+    private static void printUsage() {
+        TextResource tr = TextResource.getInstance();
+        for (int i=0; tr.getString("printAge.usage" + i) != null; i++) {
+            System.out.println(tr.getString("printAge.usage" + i));
+        }
+    }
+}
