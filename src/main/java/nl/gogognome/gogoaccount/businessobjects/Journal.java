5f0157adc12dd4da25ab7aa6a06cb7d8d7ff5773
Sander Kooijmans <sander@gogognome.nl>
2014-02-03 21:34:42 +0100

    Fixed implementation of equals() to prevent NullPointerException when updating a journal without a description.

--- src/main/java/nl/gogognome/gogoaccount/businessobjects/Journal.java
+++ src/main/java/nl/gogognome/gogoaccount/businessobjects/Journal.java
@@ -13,7 +13,7 @@
 
     You should have received a copy of the GNU General Public License
     along with gogo account.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 package nl.gogognome.gogoaccount.businessobjects;
 
 import java.util.Arrays;
@@ -21,10 +21,11 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-
 import nl.gogognome.lib.text.Amount;
 import nl.gogognome.lib.text.AmountFormat;
 import nl.gogognome.lib.util.DateUtil;
+
+import com.google.common.base.Objects;
 
 /**
  * This class represents a journal.
@@ -33,75 +34,75 @@ import nl.gogognome.lib.util.DateUtil;
  */
 public class Journal implements Comparable<Journal> {
 
-    private String id;
+	private final String id;
 
-    private String description;
+	private final String description;
 
-    private JournalItem[] items;
+	private final JournalItem[] items;
 
-    private Date date;
+	private final Date date;
 
-    /**
-     * If not <code>null</code>, then this contains the id of the invoice that is created
-     * by this journal. Journals that create an invoice can only be deleted as long as
-     * no payments have been made to the invoice.
-     */
-    private String idOfCreatedInvoice;
+	/**
+	 * If not <code>null</code>, then this contains the id of the invoice that is created
+	 * by this journal. Journals that create an invoice can only be deleted as long as
+	 * no payments have been made to the invoice.
+	 */
+	private final String idOfCreatedInvoice;
 
-    /**
-     * Creates a journal.
-     * @param id the id of the journal
-     * @param description the description of the journal
-     * @param date the date of the journal
-     * @param items the items of the journal. The sums of the debet
-     *        and credit amounts must be equal!
-     * @param idOfCreatedInvoice if not <code>null</code>, then this contains the id of the invoice
-     *        that is created by this journal
-     * @throws IllegalArgumentException if the sum of debet and credit amounts differ
-     *          or if more than one item with a party has been specified.
-     */
-    public Journal(String id, String description, Date date, List<JournalItem> items, String idOfCreatedInvoice) {
-    	this(id, description, date, items.toArray(new JournalItem[items.size()]),
-    			idOfCreatedInvoice);
-    }
+	/**
+	 * Creates a journal.
+	 * @param id the id of the journal
+	 * @param description the description of the journal
+	 * @param date the date of the journal
+	 * @param items the items of the journal. The sums of the debet
+	 *        and credit amounts must be equal!
+	 * @param idOfCreatedInvoice if not <code>null</code>, then this contains the id of the invoice
+	 *        that is created by this journal
+	 * @throws IllegalArgumentException if the sum of debet and credit amounts differ
+	 *          or if more than one item with a party has been specified.
+	 */
+	public Journal(String id, String description, Date date, List<JournalItem> items, String idOfCreatedInvoice) {
+		this(id, description, date, items.toArray(new JournalItem[items.size()]),
+				idOfCreatedInvoice);
+	}
 
-    /**
-     * Creates a journal.
-     * @param id the id of the journal
-     * @param description the description of the journal
-     * @param date the date of the journal
-     * @param items the items of the journal. The sums of the debet
-     *        and credit amounts must be equal!
-     * @param idOfCreatedInvoice if not <code>null</code>, then this contains the id of the invoice
-     *        that is created by this journal
-     * @throws IllegalArgumentException if the sum of debet and credit amounts differ
-     *          or if more than one item with a party has been specified.
-     */
-    public Journal(String id, String description, Date date, JournalItem[] items, String idOfCreatedInvoice) {
-        this.id = id;
-        this.description = description;
-        this.date = date;
-        this.items = items;
-        this.idOfCreatedInvoice = idOfCreatedInvoice;
+	/**
+	 * Creates a journal.
+	 * @param id the id of the journal
+	 * @param description the description of the journal
+	 * @param date the date of the journal
+	 * @param items the items of the journal. The sums of the debet
+	 *        and credit amounts must be equal!
+	 * @param idOfCreatedInvoice if not <code>null</code>, then this contains the id of the invoice
+	 *        that is created by this journal
+	 * @throws IllegalArgumentException if the sum of debet and credit amounts differ
+	 *          or if more than one item with a party has been specified.
+	 */
+	public Journal(String id, String description, Date date, JournalItem[] items, String idOfCreatedInvoice) {
+		this.id = id;
+		this.description = description;
+		this.date = date;
+		this.items = items;
+		this.idOfCreatedInvoice = idOfCreatedInvoice;
 
-        Amount totalDebet = null;
-        Amount totalCredit = null;
-        for (int i=0; i<items.length; i++) {
-            if (items[i].isDebet()) {
-                totalDebet = addNullable(totalDebet, items[i].getAmount());
-            } else {
-                totalCredit = addNullable(totalCredit, items[i].getAmount());
-            }
-        }
+		Amount totalDebet = null;
+		Amount totalCredit = null;
+		for (int i=0; i<items.length; i++) {
+			if (items[i].isDebet()) {
+				totalDebet = addNullable(totalDebet, items[i].getAmount());
+			} else {
+				totalCredit = addNullable(totalCredit, items[i].getAmount());
+			}
+		}
 
-        if (!nullableAmountsEqual(totalDebet, totalCredit)) {
-            AmountFormat af = new AmountFormat(Locale.getDefault());
-            throw new IllegalArgumentException(
-                    "The sum of debet and credit amounts differ for journal " + id
-                    + "! debet: " + af.formatAmount(totalDebet) +
-                    "; credit: " + af.formatAmount(totalCredit));
-        }
-    }
+		if (!nullableAmountsEqual(totalDebet, totalCredit)) {
+			AmountFormat af = new AmountFormat(Locale.getDefault());
+			throw new IllegalArgumentException(
+					"The sum of debet and credit amounts differ for journal " + id
+					+ "! debet: " + af.formatAmount(totalDebet) +
+					"; credit: " + af.formatAmount(totalCredit));
+		}
+	}
 
 	private Amount addNullable(Amount a, Amount b) {
 		if (a == null) {
@@ -113,7 +114,7 @@ public class Journal implements Comparable<Journal> {
 		}
 	}
 
-    private boolean nullableAmountsEqual(Amount a, Amount b) {
+	private boolean nullableAmountsEqual(Amount a, Amount b) {
 		if (a == null && b == null) {
 			return true;
 		} else if (a != null && b != null) {
@@ -125,91 +126,92 @@ public class Journal implements Comparable<Journal> {
 
 
 	/**
-     * Gets the date of this journal.
-     * @return the date of this journal.
-     */
-    public Date getDate()
-    {
-        return date;
-    }
+	 * Gets the date of this journal.
+	 * @return the date of this journal.
+	 */
+	public Date getDate()
+	{
+		return date;
+	}
 
-    /**
-     * Gets the description of this journal
-     * @return the description of this journal.
-     */
-    public String getDescription()
-    {
-        return description;
-    }
+	/**
+	 * Gets the description of this journal
+	 * @return the description of this journal.
+	 */
+	public String getDescription()
+	{
+		return description;
+	}
 
-    /**
-     * Gets the id of this journal.
-     * @return the id of this journal.
-     */
-    public String getId()
-    {
-        return id;
-    }
+	/**
+	 * Gets the id of this journal.
+	 * @return the id of this journal.
+	 */
+	public String getId()
+	{
+		return id;
+	}
 
-    /**
-     * Gets the items of this journal.
-     * @return the items of this journal.
-     */
-    public JournalItem[] getItems()
-    {
-        return items;
-    }
+	/**
+	 * Gets the items of this journal.
+	 * @return the items of this journal.
+	 */
+	public JournalItem[] getItems()
+	{
+		return items;
+	}
 
-    /**
-     * Gets the id of the invoice created by this journal. If no invoice was created, then
-     * <code>null</code> is returned
-     * @return the id or <code>null</code>
-     */
-    public String getIdOfCreatedInvoice() {
-        return idOfCreatedInvoice;
-    }
+	/**
+	 * Gets the id of the invoice created by this journal. If no invoice was created, then
+	 * <code>null</code> is returned
+	 * @return the id or <code>null</code>
+	 */
+	public String getIdOfCreatedInvoice() {
+		return idOfCreatedInvoice;
+	}
 
-    /**
-     * Indicates whether this journal creates an invoice.
-     * @return <code>true</code> if it creates an invoice; otherwise <code>false</code>
-     */
-    public boolean createsInvoice() {
-        return getIdOfCreatedInvoice() != null;
-    }
+	/**
+	 * Indicates whether this journal creates an invoice.
+	 * @return <code>true</code> if it creates an invoice; otherwise <code>false</code>
+	 */
+	public boolean createsInvoice() {
+		return getIdOfCreatedInvoice() != null;
+	}
 
-    /**
-     * @see java.lang.Comparable#compareTo(java.lang.Object)
-     */
-    @Override
+	/**
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	@Override
 	public int compareTo(Journal that) {
-        return this.date.compareTo(that.date);
-    }
+		return this.date.compareTo(that.date);
+	}
 
-    /**
-     * Checks whether this instance is equal to another instance.
-     * @param o the other instance
-     * @return <code>true</code> if this instance is equal to <code>o</code>;
-     *          <code>false</code> otherwise
-     */
-    @Override
+	/**
+	 * Checks whether this instance is equal to another instance.
+	 * @param o the other instance
+	 * @return <code>true</code> if this instance is equal to <code>o</code>;
+	 *          <code>false</code> otherwise
+	 */
+	@Override
 	public boolean equals(Object o) {
-        if (o instanceof Journal) {
-            Journal that = (Journal) o;
-            return this.id.equals(that.id) && this.date.equals(that.date) &&
-            	this.description.equals(that.description) &&
-            	Arrays.equals(this.items, that.items);
-        } else {
-            return false;
-        }
-    }
+		if (o instanceof Journal) {
+			Journal that = (Journal) o;
+			return Objects.equal(this.id, that.id)
+					&& Objects.equal(this.date, that.date)
+					&& Objects.equal(this.description, that.description)
+					&& Arrays.equals(this.items, that.items);
+		} else {
+			return false;
+		}
+	}
 
-    @Override
+	@Override
 	public int hashCode() {
-        return id.hashCode() + date.hashCode() + description.hashCode();
-    }
+		return id.hashCode() + date.hashCode() + description.hashCode();
+	}
 
-    @Override
-    public String toString() {
-    	return DateUtil.formatDateYYYYMMDD(date) + ' ' + id + ' ' + description;
-    }
+	@Override
+	public String toString() {
+		return DateUtil.formatDateYYYYMMDD(date) + ' ' + id + ' ' + description;
+	}
 }
