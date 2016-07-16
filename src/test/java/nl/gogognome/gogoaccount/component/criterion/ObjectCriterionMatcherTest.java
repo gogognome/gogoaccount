d449c62a8f0f9f5913843d7f383f6d8f8f82c6de
Sander Kooijmans <sander@gogognome.nl>
2016-07-16 20:35:54 +0200

    Added more testcases for ObjectCriterionMatcher. Let VarArgsMatcher use List<T> instead of T[]. That improves the readability and simplicity of usage.

--- src/test/java/nl/gogognome/gogoaccount/component/criterion/ObjectCriterionMatcherTest.java
+++ src/test/java/nl/gogognome/gogoaccount/component/criterion/ObjectCriterionMatcherTest.java
@@ -1,5 +1,6 @@
 package nl.gogognome.gogoaccount.component.criterion;
 
+import nl.gogognome.lib.util.DateUtil;
 import nl.gogognome.textsearch.criteria.Criterion;
 import nl.gogognome.textsearch.string.CriterionMatcher;
 import org.junit.Test;
@@ -20,12 +21,13 @@ public class ObjectCriterionMatcherTest {
         assertConvertsParameters(asList("bla"), asList("bla"));
         assertConvertsParameters(asList("foo", "bar"), asList("foo", "bar"));
         assertConvertsParameters(asList(123, 5.6), asList("123", "5.6"));
+        assertConvertsParameters(asList(DateUtil.createDate(2016, 7, 16)), asList("20160716", "2016-07-16", "16072016", "16-07-2016"));
     }
 
     private void assertConvertsParameters(List<Object> inputArguments, List<String> expectedArgumens) {
         CriterionMatcher criterionMatcher = mock(CriterionMatcher.class);
         ObjectCriterionMatcher matcher = new ObjectCriterionMatcher(criterionMatcher);
         matcher.matches(criterion, inputArguments.toArray(new Object[inputArguments.size()]));
-        verify(criterionMatcher).matches(any(), VarArgsMatcher.varArgEquals(expectedArgumens.toArray(new String[expectedArgumens.size()])));
+        verify(criterionMatcher).matches(any(), VarArgsMatcher.varArgEquals(expectedArgumens));
     }
 }
--- src/test/java/nl/gogognome/gogoaccount/component/criterion/VarArgsMatcher.java
+++ src/test/java/nl/gogognome/gogoaccount/component/criterion/VarArgsMatcher.java
@@ -4,32 +4,34 @@ import org.mockito.ArgumentMatcher;
 import org.mockito.internal.matchers.VarargMatcher;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Predicate;
 
 import static org.mockito.Matchers.argThat;
 
 public class VarArgsMatcher<T> extends ArgumentMatcher<T[]> implements VarargMatcher {
 
-    private final Predicate<T[]> predicate;
+    private final Predicate<List<T>> predicate;
 
-    public VarArgsMatcher(Predicate<T[]> predicate) {
+    public VarArgsMatcher(Predicate<List<T>> predicate) {
         this.predicate = predicate;
     }
 
-    public static <T> T[] varArg(Predicate<T[]> predicate) {
+    public static <T> T[] varArg(Predicate<List<T>> predicate) {
         argThat(new VarArgsMatcher<T>(predicate));
         return null;
     }
 
-    public static <T> T[] varArgEquals(T... expectedArguments) {
-        return varArg(actualArguments -> Arrays.equals(expectedArguments, actualArguments));
+    public static <T> T[] varArgEquals(List<T> expectedArguments) {
+        return varArg(actualArguments -> expectedArguments.equals(actualArguments));
     }
 
     @Override
     public boolean matches(Object argument) {
         if (argument != null && !argument.getClass().isArray()) {
-            return predicate.test((T[]) new Object[] {(T) argument});
+            return predicate.test(Collections.singletonList((T) argument));
         }
-        return predicate.test((T[]) argument);
+        return predicate.test(Arrays.asList((T[]) argument));
     }
 }
