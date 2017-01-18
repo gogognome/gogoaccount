f46b3597413f755bfb5c420aac8a4a10340dfe83
Sander Kooijmans <sander@gogognome.nl>
2017-01-18 22:34:41 +0100

    Removed some comments and use system dependent line seperator.

--- src/main/java/nl/gogognome/gogoaccount/reportgenerators/PlainTextFormat.java
+++ src/main/java/nl/gogognome/gogoaccount/reportgenerators/PlainTextFormat.java
@@ -1,40 +1,19 @@
-/*
-    This file is part of gogo account.
-
-    gogo account is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    gogo account is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with gogo account.  If not, see <http://www.gnu.org/licenses/>.
-*/
 package nl.gogognome.gogoaccount.reportgenerators;
 
 import nl.gogognome.lib.text.TextResource;
 
 /**
  * This class implements a <code>TextFormat</code> for plain text documents.
- *
- * @author Sander Kooijmans
  */
 public class PlainTextFormat extends TextFormat {
 
-    /**
-     * @param textResource
-     */
     public PlainTextFormat(TextResource textResource) {
         super(textResource);
     }
 
     @Override
 	public String getNewLine() {
-        return "\n";
+        return System.getProperty("line.separator");
     }
 
     @Override
