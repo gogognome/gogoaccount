63b55164f583ea7aaf2a5efd4d765d3102385ab8
Sander Kooijmans <sander@gogognome.nl>
2022-12-18 21:17:01 +0100

    Close extra database connection when closing document
    This might solve the occasional corruption of the database file.

--- src/main/java/nl/gogognome/gogoaccount/component/document/Document.java
+++ src/main/java/nl/gogognome/gogoaccount/component/document/Document.java
@@ -3,12 +3,15 @@ package nl.gogognome.gogoaccount.component.document;
 import nl.gogognome.dataaccess.transaction.CurrentTransaction;
 import nl.gogognome.dataaccess.transaction.Transaction;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.UUID;
+import org.slf4j.*;
 
 public class Document {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final ArrayList<DocumentListener> listeners = new ArrayList<>();
     private final String bookkeepingId = UUID.randomUUID().toString();
@@ -90,5 +93,16 @@ public class Document {
         if (readonly) {
             throw new AttemptToModifyReadonlyDocumentException();
         }
+    }
+
+    public void close() {
+        if (connectionToKeepInMemoryDatabaseAlive != null) {
+            try {
+                connectionToKeepInMemoryDatabaseAlive.close();
+            } catch (SQLException e) {
+                logger.error("A problem occurred while closing a connection to the database.", e);
+            }
+        }
+        connectionToKeepInMemoryDatabaseAlive = null;
     }
 }
